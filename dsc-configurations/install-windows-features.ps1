5db9d87741be66f7460d0141178b6bdd72f5934b
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-13 21:16:26 +0800

    Update install-windows-features.ps1

--- dsc-configurations/install-windows-features.ps1
+++ dsc-configurations/install-windows-features.ps1
@@ -1,16 +1,18 @@
+# Define the event source and log name
+$eventSource = "CustomScriptEvent"
+$logName = "Application"
 
 function Install-VmFeatures {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
         [string] $VmRole,
-
         [Parameter(Mandatory=$true)]
-        [PSCredential] $Credential,
-
+        [string] $AdminName,
+        [Parameter(Mandatory=$true)]
+        [securestring] $AdminPassword,
         [Parameter(Mandatory=$true)]
         [string] $DomainName,
-
         [Parameter(Mandatory=$true)]
         [string] $DomainBiosName
     )
@@ -18,7 +20,9 @@ function Install-VmFeatures {
     Begin {
         Write-Host 'Starting to install Windows features needed on this server.'
     }
+
     Process {
+        [pscredential] $Credential = New-Object System.Management.Automation.PSCredential($AdminName, $AdminPassword)
             try {
                 Invoke-WebRequest -Uri 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.2/PowerShell-7.3.2-win-x64.msi' -OutFile 'c:\windows\temp\PowerShell-7.3.2-win-x64.msi'
                 msiexec.exe /package 'c:\windows\temp\PowerShell-7.3.2-win-x64.msi' /passive ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
@@ -28,25 +32,47 @@ function Install-VmFeatures {
                 Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true
             }
             catch {
+                # If an error occurs, log it to the Windows event log
+                $eventLog = New-Object System.Diagnostics.EventLog($logName)
+                $eventLog.Source = $eventSource
+                $eventLog.WriteEntry($_.Exception.Message, [System.Diagnostics.EventLogEntryType]::Error)   
                 Write-Error $_.Exception.Message
             }
-
+    
             if ($vmrole -eq 'domain' || $vmrole -eq 'domaincontroller' || $vmrole -eq 'dc') {
-                Install-WindowsFeature -Name AD-Domain-Services, DNS -Credential $Credential -IncludeAllSubFeature -IncludeManagementTools
-                Import-Module ADDSDeployment 
-                Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainBiosName `
-                -DomainMode 'WinThreshold' `
-                -ForestMode 'WinThreshold' `
-                -InstallDns `
-                -SafeModeAdministratorPassword $Credential.Password `
-                -NoRebootOnCompletion `
-                -Force
-            } 
-            else
-            {
-                Install-WindowsFeature -Name Failover-Clustering, FS-FileServer, FS-DFS-Namespace, FS-DFS-Replication, FS-DFS-Service -IncludeManagementTools -Credential $credential
+                try {
+                    Install-WindowsFeature -Name AD-Domain-Services, DNS -Credential $Credential -IncludeAllSubFeature -IncludeManagementTools
+                    Import-Module ADDSDeployment 
+                    Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainBiosName `
+                    -DomainMode 'WinThreshold' `
+                    -ForestMode 'WinThreshold' `
+                    -InstallDns `
+                    -SafeModeAdministratorPassword $Credential.Password `
+                    -NoRebootOnCompletion `
+                    -Force
+                }
+                catch {
+                    # If an error occurs, log it to the Windows event log
+                    $eventLog = New-Object System.Diagnostics.EventLog($logName)
+                    $eventLog.Source = $eventSource
+                    $eventLog.WriteEntry($_.Exception.Message, [System.Diagnostics.EventLogEntryType]::Error)
+                    Write-Error $_.Exception.Message
+                }
             }
+            else {
+                try {
+                    Install-WindowsFeature -Name Failover-Clustering, FS-FileServer, FS-DFS-Namespace, FS-DFS-Replication, FS-DFS-Service -IncludeManagementTools -Credential $credential
+                }
+                catch {
+                # If an error occurs, log it to the Windows event log
+                $eventLog = New-Object System.Diagnostics.EventLog($logName)
+                $eventLog.Source = $eventSource
+                $eventLog.WriteEntry($_.Exception.Message, [System.Diagnostics.EventLogEntryType]::Error)
+                Write-Error $_.Exception.Message
+            }
+        }
     }
+    
     End {
         Write-Host "Installation of Windows features completed..."
     }
