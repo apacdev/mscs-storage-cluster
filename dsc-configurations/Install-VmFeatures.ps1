2dc1fc9a415b72fb0f0d47b9cbf5c8acc36b1237
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-15 22:47:21 +0800

    Update Install-VmFeatures.ps1

--- dsc-configurations/Install-VmFeatures.ps1
+++ dsc-configurations/Install-VmFeatures.ps1
@@ -15,12 +15,7 @@ param(
     [string] $DomainBiosName
 )
 
-$Credential = New-Object System.Management.Automation.PSCredential($AdminName, (ConvertTo-SecureString -String $AdminPass -AsPlainText -Force))
-$url = 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.2/PowerShell-7.3.2-win-x64.msi'
-
-# write function to log to Windows Event Log
 Function Write-EventLog {
-
     param(
         [Parameter(Mandatory = $true)]
         [string] $Message,
@@ -29,63 +24,67 @@ Function Write-EventLog {
         [string] $Source,
 
         [Parameter(Mandatory = $true)]
-        [string] $EventLogName
+        [string] $EventLogName,
+
+        [Parameter(Mandatory = $false)]
+        [System.Diagnostics.EventLogEntryType] $EntryType = [System.Diagnostics.EventLogEntryType]::Information
     )
 
-    $eventLog = New-Object System.Diagnostics.EventLog($EventLogName)
-    $eventLog.Source = $Source
-    $eventLog.WriteEntry($Message, [System.Diagnostics.EventLogEntryType]::Information)
+    Write-EventLog -LogName $EventLogName -Source $Source -EntryType $EntryType -EventId 1 -Message $Message
 }
 
+$eventSource = "CustomScriptEvent"
+
+if (-not [System.Diagnostics.EventLog]::SourceExists($eventSource)) {
+    [System.Diagnostics.EventLog]::CreateEventSource($eventSource, 'Application')
+}
+
+$Credential = New-Object System.Management.Automation.PSCredential($AdminName, (ConvertTo-SecureString -String $AdminPass -AsPlainText -Force))
+$url = 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.2/PowerShell-7.3.2-win-x64.msi'
+$msi = 'c:\windows\temp\PowerShell-7.3.2-win-x64.msi'
+
 if ($PSVersionTable.PSVersion.Major -lt 7) {    
-    # Install PowerShell 7 
+    
     Write-EventLog -Message 'PowerShell 7 is not installed, starting with download and installation' -Source 'CustomScriptEvent' -EventLogName 'Application'
-    Invoke-WebRequest -Uri $url -OutFile 'c:\windows\temp\PowerShell-7.3.2-win-x64.msi'
+    
+    if (-not (Test-Path -Path $msi)) {
+        Invoke-WebRequest -Uri $url -OutFile $msi
+    }
+    
     msiexec.exe /package 'c:\windows\temp\PowerShell-7.3.2-win-x64.msi' /passive ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
     Write-EventLog -Message 'Installation of PowerShell 7 is now completed.' -Source 'CustomScriptEvent' -EventLogName 'Application'
-    # Enable basic authentication and allow unencrypted traffic in WSMan
+    
     Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
     Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true
+    
     Write-EventLog -Message 'The execution of the Custom Script (WSMan Authentication) has been started.' -Source 'CustomScriptEvent' -EventLogName 'Application'
 }
 
 try {
-
     if ($VmRole -eq 'domain' -or $VmRole -eq 'domaincontroller' -or $VmRole -eq 'dc') {
-        # Install Active Directory Domain Services and promote to a domain controller
-        Install-WindowsFeature -Name AD-Domain-Services, DNS -Credential $Credential -IncludeAllSubFeature -IncludeManagementTools
-        Import-Module ADDSDeployment 
-        Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainBiosName `
-            -DomainMode 'WinThreshold' `
-            -ForestMode 'WinThreshold' `
-            -InstallDns `
-            -DomainAdministratorCredential $Credential `
-            -SafeModeAdministratorPassword $Credential.Password `
-            -Force
-            $eventLog = New-Object System.Diagnostics.EventLog("Application")
-            $eventLog.Source = "CustomScriptEvent"
-            $eventLog.WriteEntry('The execution of the Custom Script (Domain Controller) has been completed successfuly.', [System.Diagnostics.EventLogEntryType]::Information)
+        Install-WindowsFeature -Name AD-Domain-Services, DNS -IncludeAllSubFeature -IncludeManagementTools
+        # Ensure the ADDSDeployment module is installed and available
+        if (-not (Get-Module -ListAvailable -Name ADDSDeployment)) {
+            Write-Error "The ADDSDeployment module is not available. Please ensure that the Active Directory Domain Services role and its management tools are installed."
+            return
+            Import-Module ADDSDeployment
+            Install-ADDSForest -DomainName $DomainName -DomainNetbiosName $DomainBiosName `
+                -DomainMode 'WinThreshold' `
+                -ForestMode 'WinThreshold' `
+                -InstallDns `
+                -DomainAdministratorCredential $Credential `
+                -SafeModeAdministratorPassword $Credential.Password `
+                -Restart -Force
+            Write-EventLog -Message 'Installation of Active Directory Domain Services is now completed.' -Source 'CustomScriptEvent' -EventLogName 'Application' -EntryType information
+        }
     }
     else {
-        # Install failover clustering and file server features
         Install-WindowsFeature -Name Failover-Clustering, FS-FileServer -IncludeManagementTools -IncludeAllSubFeature
         Add-Computer -DomainName $DomainName -Credential $Credential -Restart
-        $eventLog = New-Object System.Diagnostics.EventLog("Application")
-        $eventLog.Source = "CustomScriptEvent"
-        $eventLog.WriteEntry('The execution of the Custom Script (Cluster Node) has been completed successfuly.', [System.Diagnostics.EventLogEntryType]::Information)
+        Write-EventLog -Message 'Windows Feature Installation has completed' -Source 'CustomScriptEvent' -EventLogName 'Application' -EntryType Information
     }
 }
 catch {
-    # If an error occurs, log it to the Windows event log and write to the console
-    $eventLog = New-Object System.Diagnostics.EventLog("Application")
-    $eventLog.Source = "CustomScriptEvent"
-    $eventLog.WriteEntry($_.Exception.Message, [System.Diagnostics.EventLogEntryType]::Error)
+    Write-EventLog -Message $_.Exception.Message -Source 'CustomScriptEvent' -EventLogName 'Application' -EntryType Error
     Write-Error $_.Exception.Message
 }
-
-# Wait for 60 seconds then Restart the computer. Mure the script resumes after the restart
-Start-Sleep -Seconds 60
-Restart-Computer -Force
-
-# Write to the Windows Event Log
-Write-EventLog -Message 'The VM has been rebooted successfuly.' -Source 'CustomScriptEvent' -EventLogName 'Application'
