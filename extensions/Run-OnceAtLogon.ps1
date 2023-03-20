2535fbca748090f44b39bfa42678f0ecfd2cf155
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-20 21:07:45 +0800

    Update Run-OnceAtLogon.ps1

--- extensions/Run-OnceAtLogon.ps1
+++ extensions/Run-OnceAtLogon.ps1
@@ -2,8 +2,8 @@ param (
         [Parameter(Mandatory = $true)]
         [string] $ServerList,
         [Parameter(Mandatory = $true)]
-        [string] $AdDomainName
+        [string] $ADDomainName
 )
 
-Write-Output $SeverList
-Write-Host $DomainName
+Write-Host [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('$ServerList'))
+Write-Host [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('$ADDomainName'))
