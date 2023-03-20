9b0a3ce507024d3071d2be91dbf5cdf5bac9e1f8
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-20 20:48:44 +0800

    Create Run-OnceAtLogon.ps1

---
+++ extensions/Run-OnceAtLogon.ps1
@@ -0,0 +1,9 @@
+param (
+        [Parameter(Mandatory = $true)]
+        [string] $ServerList,
+        [Parameter(Mandatory = $true)]
+        [string] $DomainName
+)
+
+Write-Host $SeverList
+Write-Host $DomainName
