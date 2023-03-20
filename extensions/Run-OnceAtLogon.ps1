77d72d6ecd14e447bf68714d1253de2c88c6ff26
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-20 21:12:13 +0800

    Update Run-OnceAtLogon.ps1

--- extensions/Run-OnceAtLogon.ps1
+++ extensions/Run-OnceAtLogon.ps1
@@ -5,5 +5,38 @@ param (
         [string] $ADDomainName
 )
 
-Write-Host [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('$ServerList'))
-Write-Host [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('$ADDomainName'))
+Function Write-EventLog {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Message,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Source,
+
+        [Parameter(Mandatory = $true)]
+        [string] $EventLogName,
+
+        [Parameter(Mandatory = $false)]
+        [System.Diagnostics.EventLogEntryType] $EntryType = [System.Diagnostics.EventLogEntryType]::Information
+    )
+    
+    $log = New-Object System.Diagnostics.EventLog($EventLogName)
+    $log.Source = $Source
+    $log.WriteEntry($Message, $EntryType)
+}
+
+
+$EventSource = "CustomScriptEvent"
+$EventLogName = "Application"
+
+# Check whether the event source exists, and create it if it doesn't exist.
+if (-not [System.Diagnostics.EventLog]::SourceExists($EventSource)) {
+    [System.Diagnostics.EventLog]::CreateEventSource($EventSource, $EventLogName)
+}
+
+$list = Write-Host [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('$ServerList'))
+$name = Write-Host [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('$ADDomainName'))
+
+Write-Output $name
+Write-EventLog -Message $_.Exception.Message -Source $EventSource -EventLogName $EventLogName -EntryType Error
+
