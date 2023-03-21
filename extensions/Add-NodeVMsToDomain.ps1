d12479df9475d72fddb17fb36713fb98524208c3
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-21 22:29:32 +0800

    Create Add-NodeVMsToDomain.ps1

---
+++ extensions/Add-NodeVMsToDomain.ps1
@@ -0,0 +1,14 @@
+param(
+    [Parameter(Mandatory=$true)] [string] $DomainName,
+    [Parameter(Mandatory=$true)] [string] $DomainServerIpAddress,
+    [Parameter(Mandatory=$true)] [string] $AdminUserName,
+    [Parameter(Mandatory=$true)] [securestring] $AdminSecret
+)
+
+Set-DnsClientServerAddress -InterfaceIndex ((Get-NetAdapter -Name "Ethernet").ifIndex) `
+    -ServerAddresses $DomainServerIpAddress
+
+Add-Computer -DomainName $DomainName `
+    -Credential (New-Object System.Management.Automation.PSCredential($AdminUser, $AdminSecret)) `
+    -Restart `
+    -Force
