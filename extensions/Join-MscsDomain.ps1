b45291c9f0575e2ef99319b2c89254c2da192b51
Patrick Shim <47954319+ms-apac-csu@users.noreply.github.com>
2023-03-29 13:27:17 +0800

    Update Join-MscsDomain.ps1

--- extensions/Join-MscsDomain.ps1
+++ extensions/Join-MscsDomain.ps1
@@ -4,40 +4,28 @@ param (
     [Parameter(Mandatory = $true)] [string] $AdminName,
     [Parameter(Mandatory = $true)] [string] $AdminPass
 )
- 
-$EventSource = "CustomScriptEvent"
-$EventLogName = "Application"
-$MaxRetries = 30
-$RetryIntervalSeconds = 1
-
-$Credential = New-Object System.Management.Automation.PSCredential($AdminName, (ConvertTo-SecureString -String $AdminPass -AsPlainText -Force))
- 
-# Check whether the event source exists, and create it if it doesn't exist.
-if (-not [System.Diagnostics.EventLog]::SourceExists($EventSource)) {
-    [System.Diagnostics.EventLog]::CreateEventSource($EventSource, $EventLogName)
-}
 
 Function Write-EventLog {
     param(
-            [Parameter(Mandatory = $true)]
-            [string] $Message,
+        [Parameter(Mandatory = $true)]
+        [string] $Message,
 
-            [Parameter(Mandatory = $true)]
-            [string] $Source,
+        [Parameter(Mandatory = $true)]
+        [string] $Source,
 
-            [Parameter(Mandatory = $true)]
-            [string] $EventLogName,
+        [Parameter(Mandatory = $true)]
+        [string] $EventLogName,
 
-            [Parameter(Mandatory = $false)]
-            [System.Diagnostics.EventLogEntryType] $EntryType = [System.Diagnostics.EventLogEntryType]::Information
-        )
+        [Parameter(Mandatory = $false)]
+        [System.Diagnostics.EventLogEntryType] $EntryType = [System.Diagnostics.EventLogEntryType]::Information
+    )
     
     $log = New-Object System.Diagnostics.EventLog($EventLogName)
     $log.Source = $Source
     $log.WriteEntry($Message, $EntryType)
 
     # Set log directory and file
-    $logDirectory = "C:\temp\cselogs"
+    $logDirectory = "C:\\Temp\\CseLogs"
     $logFile = Join-Path $logDirectory "$(Get-Date -Format 'yyyyMMdd').log"
 
     # Create the log directory if it does not exist
@@ -53,43 +41,54 @@ Function Write-EventLog {
     Add-Content -Path $logFile -Value $logEntry
 }
 
-  Write-EventLog -Message "Joining domain $DomainName" -Source $EventSource -EventLogName $EventLogName -EntryType Information
+$EventSource = "CustomScriptEvent"
+$EventLogName = "Application"
+$MaxRetries = 30
+$RetryIntervalSeconds = 1
+$TargetDns = "www.google.com"
 
-  $retries = 0
+# Check whether the event source exists, and create it if it doesn't exist.
+if (-not [System.Diagnostics.EventLog]::SourceExists($EventSource)) {
+    [System.Diagnostics.EventLog]::CreateEventSource($EventSource, $EventLogName)
+}
 
-  # Wait for network connectivity to the domain server
-  
-  while ($retries -lt $MaxRetries) {
-      if ( $true -ne (Test-NetConnection -ComputerName $DomainServerIpAddress -Port 389)) {
-          Write-EventLog -Message "Network connectivity to domain controller $DomainServerIpAddress not established. Retrying in $RetryIntervalSeconds seconds." -Source $EventSource -EventLogName $EventLogName -EntryType Information
-          Start-Sleep -Seconds $RetryIntervalSeconds
-          $retries++
-      }
-      else {
-          Write-EventLog -Message "Network connectivity to domain controller $DomainServerIpAddress is OK." -Source $EventSource -EventLogName $EventLogName -EntryType Information
+$Credential = New-Object System.Management.Automation.PSCredential($AdminName, (ConvertTo-SecureString -String $AdminPass -AsPlainText -Force))
 
-          Set-DnsClientServerAddress -InterfaceIndex ((Get-NetAdapter -Name "Ethernet").ifIndex) -ServerAddresses $DomainServerIpAddress
-          Clear-DnsClientCache
+Write-EventLog -Message "Joining domain $DomainName" -Source $EventSource -EventLogName $EventLogName -EntryType Information
+$retries = 0
+
+# Wait for network connectivity to the domain server
+while ($retries -lt $MaxRetries) {
+    if ( $true -ne (Test-NetConnection -ComputerName $DomainServerIpAddress -Port 389)) {
+        Write-EventLog -Message "Network connectivity to domain controller $DomainServerIpAddress not established. Retrying in $RetryIntervalSeconds seconds." -Source $EventSource -EventLogName $EventLogName -EntryType Information
+        Start-Sleep -Seconds $RetryIntervalSeconds
+        $retries++
+    }
+    else {
+        Write-EventLog -Message "Network connectivity to domain controller $DomainServerIpAddress is OK." -Source $EventSource -EventLogName $EventLogName -EntryType Information
+        Set-DnsClientServerAddress -InterfaceIndex ((Get-NetAdapter -Name "Ethernet").ifIndex) -ServerAddresses $DomainServerIpAddress
+        Clear-DnsClientCache
           
-          Test-NetConnection -ComputerName 'www.google.com'
-          Test-NetConnection -ComputerName $DomainName
+        Test-NetConnection -ComputerName $TargetDns
+        Test-NetConnection -ComputerName $DomainName
 
-          # Join domain
-          Write-EventLog -Message "Joining domain $DomainName" -Source $EventSource -EventLogName $EventLogName -EntryType Information
-          try {
-              Add-Computer -ComputerName $env:COMPUTERNAME `
-                  -LocalCredential $Credential `
-                  -DomainName $DomainName `
-                  -Credential $Credential `
-                  -Force
+        # Join domain
+        Write-EventLog -Message "Joining domain $DomainName" -Source $EventSource -EventLogName $EventLogName -EntryType Information
+        try {
+            Add-Computer -ComputerName $env:COMPUTERNAME `
+                -LocalCredential $Credential `
+                -DomainName $DomainName `
+                -Credential $Credential `
+                -Restart `
+                -Force
 
-              Write-EventLog -Message "Joined domain $DomainName. Now restarting the computer." -Source $EventSource -EventLogName $EventLogName -EntryType Information
-              break
-          }
-          catch {
-              Write-EventLog -Message "Failed to join domain $DomainName. Retrying in $RetryIntervalSeconds seconds." -Source $EventSource -EventLogName $EventLogName -EntryType Information
-              Start-Sleep -Seconds $RetryIntervalSeconds
-              $retries++
-          }
-      }
-  }
+            Write-EventLog -Message "Joined domain $DomainName. Now restarting the computer." -Source $EventSource -EventLogName $EventLogName -EntryType Information
+            break
+        }
+        catch {
+            Write-EventLog -Message "Failed to join domain $DomainName. Retrying in $RetryIntervalSeconds seconds." -Source $EventSource -EventLogName $EventLogName -EntryType Error
+            Start-Sleep -Seconds $RetryIntervalSeconds
+            $retries++
+        }
+    }
+}
